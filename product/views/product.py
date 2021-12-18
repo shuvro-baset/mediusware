dd05854a533c9b7653fe1aaca4697fe4a459af8a
shuvro baset <md.abdul.baset75@gmail.com>
2021-12-19 00:11:22 +0600

    name and price filtering done

--- product/views/product.py
+++ product/views/product.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.views import generic
 from django.views.generic import ListView, CreateView, UpdateView
-
+from django.core.paginator import Paginator
 from product.models import Variant, Product, ProductImage, ProductVariantPrice
 
 
@@ -27,23 +27,29 @@ class ProductsView(generic.TemplateView):
     def get(self, request):
         products = ProductVariantPrice.objects.all()
         print("products: ", products)
+        # todo: pagination start
+        paginator = Paginator(products, 10)
+        page_number = request.GET.get("page", 1)
+        page_obj = paginator.get_page(page_number)
+        print("ad", page_obj, paginator)
+        # todo: pagination end
         context = {
-            'products': products
+            # 'products': products,
+            "page_obj": page_obj
         }
         return render(request, self.template_name, context=context)
 
     def post(self, request):
         if request.method == "POST":
             name = request.POST.get('title')
+            price_from = request.POST.get('price_from')
+            price_to = request.POST.get('price_to')
             print("name: ", name)
             product_ins = ProductVariantPrice.objects.all()
-            products = product_ins.filter(product_id__title=name)
+            products = product_ins.filter(product__title__icontains=name, price__gte=price_from, price__lte=price_to)
             print(products)
 
-        context = {
-            'products': products
-        }
-        return render(request, 'products/filtered_products.html', context=context)
-
-# class FilterProductsView(generic.TemplateView):
-#     template_name = 'products/filtered_products.html'
+            context = {
+                'products': products
+            }
+            return render(request, 'products/filtered_products.html', context=context)
