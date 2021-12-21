fd6c303242d7b2316405c3043730d277eb13ec7d
shuvro baset <md.abdul.baset75@gmail.com>
2021-12-21 23:07:20 +0600

    single product data show done

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from django.views.generic import TemplateView
 
-from product.views.product import CreateProductView, ProductsView
+from product.views.product import CreateProductView, ProductsView, ProductsEditView
 from product.views.variant import VariantView, VariantCreateView, VariantEditView
 
 app_name = "product"
@@ -15,5 +15,6 @@ urlpatterns = [
     # Products URLs
     path('create/', CreateProductView.as_view(), name='create.product'),
     path('list/', ProductsView.as_view(), name='list.product'),
+    path('product/<int:id>/edit', ProductsEditView.as_view(), name='update.product'),
 
 ]
--- product/views/product.py
+++ product/views/product.py
@@ -5,6 +5,7 @@ from django.core.paginator import Paginator
 from product.models import Variant, Product, ProductImage, ProductVariantPrice, ProductVariant
 from django.db.models import Q
 
+
 class BaseProductView(generic.View):
     model = Product
     template_name = 'products/list.html'
@@ -20,6 +21,7 @@ class CreateProductView(generic.TemplateView):
         context['variants'] = list(variants.all())
         return context
 
+
 # showing products and filter products
 class ProductsView(generic.TemplateView):
     template_name = 'products/list.html'
@@ -30,11 +32,21 @@ class ProductsView(generic.TemplateView):
         # getting variants
         variants = ProductVariant.objects.all()
         colors = []
+        sizes = []
+        styles = []
         for v in variants:
-            if v.variant_id==1:
+            if v.variant_id == 1:
                 color = v.variant_title
                 if color not in colors:
                     colors.append(color)
+            if v.variant_id == 2:
+                size = v.variant_title
+                if size not in sizes:
+                    sizes.append(size)
+            if v.variant_id == 3:
+                style = v.variant_title
+                if style not in styles:
+                    styles.append(style)
         print("variants: ", colors)
         # todo: pagination start
         paginator = Paginator(products, 10)
@@ -44,7 +56,9 @@ class ProductsView(generic.TemplateView):
         context = {
             # 'products': products,
             "page_obj": page_obj,
-            "colors": colors
+            "colors": colors,
+            "sizes": sizes,
+            "styles": styles,
         }
         return render(request, self.template_name, context=context)
 
@@ -55,9 +69,10 @@ class ProductsView(generic.TemplateView):
             name = request.POST.get('title')
             price_from = request.POST.get('price_from')
             price_to = request.POST.get('price_to')
+            variant = request.POST.get('variant')
             product_ins = ProductVariantPrice.objects.all()
             # condition for not provided price
-            if None or '' in [price_to, price_from]:
+            if None or '' in [price_to, price_from, variant]:
                 products = product_ins.filter(product__title__icontains=name)
                 paginator = Paginator(products, 10)
                 page_number = request.GET.get("page", 1)
@@ -68,7 +83,8 @@ class ProductsView(generic.TemplateView):
                 }
                 return render(request, 'products/filtered_products.html', context=context)
             else:
-                products = product_ins.filter(Q(product__title__icontains=name) | Q(price__gte=price_from) | Q(price__lte=price_to))
+                products = product_ins.filter(
+                    Q(product__title__icontains=name) | Q(price__gte=price_from) | Q(price__lte=price_to))
 
                 paginator = Paginator(products, 10)
                 page_number = request.GET.get("page", 1)
@@ -78,3 +94,16 @@ class ProductsView(generic.TemplateView):
                     'page_obj': page_obj
                 }
                 return render(request, 'products/filtered_products.html', context=context)
+
+
+class ProductsEditView(generic.TemplateView):
+    template_name = 'products/single_product.html'
+
+    def get(self, request, id):
+        product = ProductVariantPrice.objects.get(id=id)
+
+        context = {
+            "product": product
+        }
+        # print(product.price)
+        return render(request, 'products/single_product.html', context=context)
